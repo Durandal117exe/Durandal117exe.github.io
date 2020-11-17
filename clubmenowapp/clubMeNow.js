5208f3bab5061d938134a05b111f7336e436f150
Durandal117exe <71295512+Durandal117exe@users.noreply.github.com>
2020-11-16 20:24:46 -0500

    added stuff (don't ask me)

--- clubmenowapp/clubMeNow.js
+++ clubmenowapp/clubMeNow.js
@@ -128,7 +128,8 @@ function resetAllClubDistances() {
 // navigate to "About" screen
 function displayAbout() {
 	// your code here
-	// window.location.href = "clubAbout.html";
+	 window.location.href = "clubAbout.html";
+	 
 }
 
 // navigate to "Penalty Info" screen
